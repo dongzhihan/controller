01a1fe2af07113b0d88f2ff9ef4d6d847826beea
dongzhihan <529211933@qq.com>
2017-05-05 17:48:03 +0800

    2017年5月5日17:48:00

--- src/components/pages/carCtl.vue
+++ src/components/pages/carCtl.vue
@@ -5,15 +5,15 @@
       <cell title="小车详细" :value="$route.params.id"></cell>
       <cell title="小车状态" :value="carState"></cell>
     </group>
-    <div class="center"  @click="carClick('forward')"><img class="btnColor" src="../../image/top.png"></img>
+    <div class="center" @click="carClick('forward')"><img class="btnColor" src="../../image/top.png"></img>
     </div>
 
-    <div   style="display: flex;justify-content: center">
-      <div class=""  @click="carClick('left')"><img class="btnColor" src="../../image/left.png"></div>
+    <div style="display: flex;justify-content: center">
+      <div class="" @click="carClick('left')"><img class="btnColor" src="../../image/left.png"></div>
       <div style="width:1rem"></div>
-      <div   @click="carClick('right')"><img class="btnColor" src="../../image/right.png"></div>
+      <div @click="carClick('right')"><img class="btnColor" src="../../image/right.png"></div>
     </div>
-    <div class="center"   @click="carClick('back')"><img class="btnColor" src="../../image/down.png"></img>
+    <div class="center" @click="carClick('back')"><img class="btnColor" src="../../image/down.png"></img>
     </div>
   </div>
 </template>
@@ -21,10 +21,11 @@
   .center {
     text-align: center;
   }
-.btnColor:active
-{ 
-background-color:greenyellow;
-}
+
+  .btnColor:active {
+    background-color: greenyellow;
+  }
+
 </style>
 <script>
   import {
@@ -64,6 +65,6 @@ background-color:greenyellow;
 </script>
 
 <style scoped>
- 
+
 
 </style>
